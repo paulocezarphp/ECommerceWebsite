e0177a3765bd0f0c50d741a109c376cb46c62b73
Paulo Cezar <32718867+paulocezarphp@users.noreply.github.com>
2017-11-13 19:15:07 -0200

    Versão 0.5
    Dessa vez coloquei alguns outras funcionalidades como colocar elementos dentro do carrinho e criei um MenuBar para exibir os elementos na tela.

--- ECommerceWebsite/public_html/javascript/controll.js
+++ ECommerceWebsite/public_html/javascript/controll.js
@@ -4,4 +4,81 @@
  * and open the template in the editor.
  */
 
+$(document).ready(function(){
+
+    var carrinho = 0;
+    var nocarrinho = [];   
+    var controlebar = 0;
+    
+    /*
+     * Aqui nós atualizamos o numero de elementos do carrinho
+     */
+    
+    function AtualizarCarrinho(){
+        
+        $("#elemento_carrinho").text(carrinho);
+        
+    }
+
+    /*
+     * Aqui nós adicionamos um elemento dentro do carrinho
+     */
+    
+    function AddCarrinho(){
+        
+        carrinho = carrinho + 1;
+        
+    }
+
+     /*
+     * Aqui nós removemos um elemento dentro do carrinho
+     */
+    
+    function RemCarrinho(){
+        
+        carrinho = carrinho - 1;
+        
+    }
+    
+    /*
+     * Aqui nós colocamos o elemento dentro da lista do carrinho 
+     */
+    
+    $(".evento_comprar").click(function() {
+       
+       AddCarrinho();
+       AtualizarCarrinho(); 
+       
+       $("#naLista").append("<div class='elementoLista'>Comprou</div>");
+       
+    });
+    
+    /*
+     * Aqui nós controlamos o MenuBar que exibe os elementos da lista 
+     */
+    
+    $("#mais").click(function() {
+        
+       if(controlebar > 1){
+           controlebar = 0;
+       }
+       
+       
+       if(controlebar === 0){
+           
+          $("#naLista").hide("slow");  
+            
+       }
+       else{
+         
+         $("#naLista").show("slow");
+            
+       }    
+                    
+       controlebar++;   
+       
+       
+    });
+
+});
 
